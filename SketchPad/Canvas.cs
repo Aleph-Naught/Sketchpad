8a1bcfcc52c20329efd34a7746decf154c0fa5af
narwhaldisco <nward24@uwo.ca>
2014-03-30 20:03:47 -0400

    wut

---
+++ SketchPad/Canvas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SketchPad
+{
+    public class Canvas : Panel
+    {
+        public Canvas() {
+        this.DoubleBuffered = true;
+        this.ResizeRedraw = true;
+    }
+
+    }
+}
